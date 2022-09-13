2ffffb9204df065faca699fa9ca302c634b74826
sunpm <15022352@qq.com>
2022-09-13 17:27:51 +0800

    添加《模版语法》示例代码

---
+++ src/pages/01.essentials/01.template-syntax.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>文本插值</h2>
+  <span>Message: {{ msg }}</span>
+  <hr>
+  <h2>原始html</h2>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <hr>
+  <h2>Attribute 绑定</h2>
+  <div v-bind:id="dynamicId"></div>
+  <div :id="dynamicId"></div>
+  <hr>
+  <h2>布尔型 Attribute</h2>
+  <button :disabled="isButtonDisabled">Button</button>
+  <hr>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('文本插值')
+
+const rawHtml = ref(`<span style="color: red">This should be red.</span>`)
+
+const dynamicId = ref('dynamic-id')
+
+const isButtonDisabled = ref(true)
+</script>
+
+<style scoped>
+
+</style>
